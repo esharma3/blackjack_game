28c479c46abcfa3c77bcd2d4e0142ff9b918debc
Ekta Sharma <ekta.sharma3@gmail.com>
2020-02-05 22:21:48 -0600

    added print stmts, removed extra code from dealer class

--- blackjack_challenge.py
+++ blackjack_challenge.py
@@ -1,9 +1,12 @@
+# Blackjack game (excluding split and double) implemented using python classes. 
+# =============================================================================
+
 # importing dependencies
 import os
 import random
 
-# creating class for card values and suit
-class Card():
+# creating class for card values and suits
+class Card:
 
     def __init__(self, value, suit):
         self.value = value
@@ -15,15 +18,15 @@ class Card():
 
 
 # creating class for building the deck (inheriting from Card class), shuffling the deck and drawing a card
-class Deck():
+class Deck:
 
     def __init__(self):
 
         values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-        
+        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]        
         self.cards = []
-    
+
+        # building deck of cards
         for suit in suits:
             for value in values:
                 card = Card(value, suit)
@@ -41,8 +44,8 @@ class Deck():
         return f"{self.cards}"
 
 
-# creating class for player hand and player total score (inheriting from Deck class)
-class Player():
+# creating class for player hand and player total score
+class Player:
 
     def __init__(self):
         self.hand = []
@@ -75,26 +78,20 @@ class Player():
         return self.sum
 
     def __repr__(self):
-        return f'Player Cards: [{"][".join(self.hand)}] ---> {self.sum}'
+        return f'Player Cards: [{"][".join(self.hand)}] -----> {self.sum}'
 
 
-# creating class for dealer hand and dealer total score (inheriting from Player and Deck class)
+
+# creating class for dealer hand and dealer total score (inheriting from Player class)
 class Dealer(Player):
-
-    def __init__(self):
-        super().__init__()
-        return
-
-    def __repr__(self):
-        return f'Dealer Cards: [{"][".join(self.hand)}] ---> {self.sum}'
+    pass
 
 
 # creating class for playing the game of Blackjack (includes functions for hit and stand)
-class Play():
+class Play:
 
     def __init__(self):
         self.deck = Deck()
-        self.cards = self.deck.cards
         self.deck.shuffle()
         self.player = Player()
         self.dealer = Dealer()
@@ -107,6 +104,8 @@ class Play():
         return
 
     def play_blackjack(self):
+
+        # adding first hand
         self.player.add_card(self.deck)
         self.dealer.add_card(self.deck)
         self.player.add_card(self.deck)
@@ -118,6 +117,10 @@ class Play():
             self.p_score = self.player.score()
             self.d_score = self.dealer.score()
 
+            print("---------------------------------")
+            print("Welcome to the game of BlackJack!")
+            print("---------------------------------")
+            print("")
             if self.standing:
                 print(self.dealer)
             else:
@@ -138,7 +141,7 @@ class Play():
                 break
 
             if self.first_hand and self.p_score == 21:
-                print("BlackJack! Game Over!")
+                print("It is BlackJack! Game Over!")
                 break
 
             self.first_hand = False
@@ -181,11 +184,15 @@ if __name__ == "__main__":
 play_again = "Y"
 
 while play_again:
-    print("")
-    play_again = input("Do you want to play again? Enter Y/N: ") 
+
+    play_again = input("\nDo you want to play again? Enter Y/N: ")
+
     if play_again.lower() == "y":
         play = Play()
         play.play_blackjack()
-    else:
+    elif play_again.lower() == "n":
+        print("\nGood Bye!")
         break
-        
+    else:
+        print("\nSorry, that is not a valid option. Try again!")
+        pass
